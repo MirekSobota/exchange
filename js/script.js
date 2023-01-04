fc9f177aad4de458c4ff1f22179a8358dccd804b
MirekSobota <miroslaw.sobota@gmail.com>
2023-01-04 22:23:11 +0100

    Working on JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 {
 
-    const formElement = document.querySelector(".js-form");
+    let currencyChoice;
     const currencyFocusElement = document.querySelector(".js-currencyType");
+
+    currencyFocusElement.addEventListener("change", (event) =>{
+        
+         currencyChoice = currencyFocusElement.value;
+         return currencyChoice;
+    })
+
+    
 
     const euro = 4.69;
     const usd = 4.51;
     const gbp = 5.45;
     const btc = 72000;
     const svc = 1.94;
+
+
 
     const exchangeRate = () => {
         const rateEuroElement = document.querySelector(".js-rateEuro");
@@ -23,48 +33,44 @@
         rateSvcElement.innerText = svc;
     }
 
+    const calculatingResult = (currencyChoice) => {
+        const resultElement = document.querySelector(".js-result");
+        const amountElement = document.querySelector(".js-amount");
+        const amount = +amountElement.value;
+
+        const rate = calculatingValue(currencyChoice);
+
+        resultElement.innerText = `${amount.toFixed(2)} PLN =  ${(amount / rate).toFixed(2)} ${currencyChoice}`;
+    }
+
+
+    const calculatingValue = (currencyChoice) => {
+
+        switch (currencyChoice) {
+            case "EUR":
+                return euro;
+
+            case "USD":
+                return usd;
+
+            case "GBP":
+                return gbp;
+
+            case "BTC":
+                return btc;
+
+            case "SVC":
+                return svc;
+
+        }
+    }
+
+    const formElement = document.querySelector(".js-form");
+
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        
-        
-
-
-        const calculatingValue = (currencyChoice) => {
-
-            switch (currencyChoice) {
-                case "EUR":
-                    return euro;
-                    
-                case "USD":
-                    return usd;
-                    
-                case "GBP":
-                    return gbp;
-            
-                case "BTC":
-                    return btc;
-
-                case "SVC":
-                    return svc;
-                
-            }
-        }
-
-
-
-        const calculatingResult = () => {
-            const resultElement = document.querySelector(".js-result");
-            const amountElement = document.querySelector(".js-amount");
-            const amount = +amountElement.value;
-            const currencyChoice = currencyFocusElement.value;
-            const rate = calculatingValue(currencyChoice);
-
-            resultElement.innerText = `${amount.toFixed(2)} PLN =  ${(amount / rate).toFixed(2)} ${currencyChoice}`;
-        }
-
-        
-        calculatingResult();
+        calculatingResult(currencyChoice);
     })
 
     const init = () => {
