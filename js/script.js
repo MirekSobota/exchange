b6b7c264ff432e18e6d21dea4593d037bdb785d4
MirekSobota <miroslaw.sobota@gmail.com>
2023-01-05 10:34:48 +0100

    Refactoring JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,10 @@
 {
 
-    let currencyChoice;
-    const currencyFocusElement = document.querySelector(".js-currencyType");
-
-    currencyFocusElement.addEventListener("change", (event) =>{
-        
-         currencyChoice = currencyFocusElement.value;
-         return currencyChoice;
-    })
-
-    
-
     const euro = 4.69;
     const usd = 4.51;
     const gbp = 5.45;
     const btc = 72000;
     const svc = 1.94;
-
-
 
     const exchangeRate = () => {
         const rateEuroElement = document.querySelector(".js-rateEuro");
@@ -37,12 +24,9 @@
         const resultElement = document.querySelector(".js-result");
         const amountElement = document.querySelector(".js-amount");
         const amount = +amountElement.value;
-
         const rate = calculatingValue(currencyChoice);
-
         resultElement.innerText = `${amount.toFixed(2)} PLN =  ${(amount / rate).toFixed(2)} ${currencyChoice}`;
     }
-
 
     const calculatingValue = (currencyChoice) => {
 
@@ -69,6 +53,9 @@
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
+
+        const currencyFocusElement = document.querySelector(".js-currencyType");
+        let currencyChoice = currencyFocusElement.value;
 
         calculatingResult(currencyChoice);
     })
